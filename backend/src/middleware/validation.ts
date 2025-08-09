037f3c61f93629fc2900eee4972f61d9f8bb646e
Daniel <finestdan1979@gmail.com>
2025-08-09 17:45:05 +0100

    done with cloudinary setup, render setup and create restaurant set-up. Next up: Create restaurant form

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -24,3 +24,29 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required!"),
+  body("city").notEmpty().withMessage("city is required!"),
+  body("country").notEmpty().withMessage("country is required!"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery Price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("estimatedDeliveryTime must be a positive integer!"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array!")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty!"),
+  body("menuItems").isArray().withMessage("Menu Items Must Be An Array!"),
+  body("menuItems.*.name")
+    .notEmpty()
+    .withMessage("Menu Item Name Is Required!"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu Item Price Is Required And Must Be A Positive Number!"),
+  handleValidationErrors,
+];
