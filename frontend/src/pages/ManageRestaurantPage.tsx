f9eac08fa497d88ea404f846f9c5416c5469fecc
Daniel <finestdan1979@gmail.com>
2025-08-15 16:40:20 +0100

    Done with create restaurant api. Up next: create get my restaurant api

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,7 +1,11 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isPending } = useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isPending={isPending} />
+  );
 };
 
 export default ManageRestaurantPage;
