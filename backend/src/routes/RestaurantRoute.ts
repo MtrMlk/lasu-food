449e6b73fad3e9cb6eace9a6f9818a44c62fbc18
Daniel <finestdan1979@gmail.com>
2025-09-01 13:57:01 +0100

    implemented search in be

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
 const router = express.Router();
 
-// api/restuarant/search/city
+// api/restuarant/search/campus
 router.get(
-  "/search/:city",
-  param("city")
+  "/search/:campus",
+  param("campus")
     .isString()
     .trim()
     .notEmpty()
-    .withMessage("City parameter must be a valid string")
+    .withMessage("City parameter must be a valid string"),
+  RestaurantController.searchRestaurant
 );
+
+export default router;
